630b5a86b9895730a17024f5533621eb1ae98bef
FrancescoPepi <francescopepi16@gmail.com>
2023-10-18 17:27:10 +0200

    error seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = Config("comics");
+        foreach ($comics as $book) {
+            $newComics = new Comic();
+            $newComics->title = $book['title'];
+            $newComics->title = $book['description'];
+            $newComics->title = $book['thumb'];
+            $newComics->title = $book['price'];
+            $newComics->title = $book['series'];
+            $newComics->title = $book['sale_date'];
+            $newComics->title = $book['type'];
+            $newComics->save();
+        }
     }
 }
