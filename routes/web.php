e05639e613e696421bf950a1d0dc22f6ca58e8a2
FrancescoPepi <francescopepi16@gmail.com>
2023-10-18 16:31:16 +0200

    feat controller ; model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\PageController;
+// use App\Http\Controllers\PageController;
+use App\Http\Controllers\ComicController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name('home');
+// Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/comics', [ComicController::class, 'index'])->name('comic.index');
