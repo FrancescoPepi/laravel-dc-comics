ff661b19b68f27bfc095a1ff575bd42bb442197b
FrancescoPepi <francescopepi16@gmail.com>
2023-10-19 17:21:28 +0200

    feat control delete button

--- resources/views/partials/_listComics.blade.php
+++ resources/views/partials/_listComics.blade.php
@@ -35,13 +35,36 @@
                             </a>
                         </div>
                         <div>
-                            <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn mx-1">
-                                    <i class="fa-solid fa-trash-arrow-up" style="color: #d00606!important;"></i>
-                                </button>
-                            </form>
+                            <button class="btn mx-1" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal{{ $comic->id }}">
+                                <i class="fa-solid fa-trash-arrow-up" style="color: #d00606!important;"></i>
+                            </button>
+
+                            <div class="modal fade" id="exampleModal{{ $comic->id }}" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina prodotto</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Sei sicuro di voler eliminare <strong>"{{ $comic->title }}"</strong> dalla
+                                            lista?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Annulla</button>
+                                            <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-danger">Elimina</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </td>
